258a900844a164e4ed0b6021de3326d04662a5bc
asanzroc <aaron.sanz-roca-external@capgemini.com>
2022-07-05 13:02:10 +0200

    mostrar pedidos en usuario

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', '3f607e7606fce8e1bbfc6060aab47c43'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
